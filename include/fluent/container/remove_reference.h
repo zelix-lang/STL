4d7e61f1c08265efe20319296e27378a6c32b1ea
rodrigoo-r <2nw53j32643y@gmail.com>
2025-07-30 20:36:30 -0500

    Add remove_reference utility for type manipulation

---
+++ include/fluent/container/remove_reference.h
@@ -0,0 +1,59 @@
+/*
+        ==== The Fluent Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Fluent Programming Language
+    codebase. Fluent is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly performant.
+---------------------------------------------------------
+  - Fluent is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+---------------------------------------------------------
+  - Fluent is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even
+    the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+---------------------------------------------------------
+  - You should have received a copy of the GNU General
+    Public License along with Fluent. If not, see
+    <https://www.gnu.org/licenses/>.
+*/
+
+//
+// Created by rodrigo on 7/30/25.
+//
+
+#pragma once
+
+namespace fluent::container
+{
+#if _GLIBCXX_USE_BUILTIN_TRAIT(__remove_reference)
+    template<typename T>
+    struct remove_reference
+    {
+        using type = __remove_reference(T);
+    };
+#else
+    template<typename T>
+    struct remove_reference
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_reference<T&>
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_reference<T&&>
+    {
+        using type = T;
+    };
+#endif
+}
