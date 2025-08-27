d5a769f685be33302c840b608aac967703da071f
rodrigoo-r <rodrigo@zelixlang.dev>
2025-08-26 21:14:39 -0500

    Refactor push_back method in vector.h to use perfect forwarding for improved performance

--- include/zelix/container/vector.h
+++ include/zelix/container/vector.h
@@ -191,34 +191,10 @@ namespace zelix::stl
              *
              * @param value Element to append.
              */
-            void push_back(T &value)
+            template <class U = T>
+            void push_back(U &&value)
             {
-                emplace_back(stl::move(value));
-            }
-
-            /**
-             * @brief Appends a copy of the given element to the end of the vector.
-             *
-             * @param value Element to append.
-             */
-            void push_back(T &&value)
-            {
-#           if defined(__GNUC__) || defined(__clang__)
-                if (__builtin_expect(initialized_ == 0, 0))
-#           else
-                if (!initialized_)
-#           endif
-                {
-                    init();
-                }
-
-                if (size_ >= capacity_)
-                {
-                    resize(static_cast<size_t>(capacity_ * GrowthFactor));
-                }
-
-                new (&this->data[size_]) T(stl::move(value));
-                ++size_;
+                emplace_back(stl::forward<T>(value));
             }
 
             /**
