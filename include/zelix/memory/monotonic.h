1d4a579d7b8d67c543fe0dc78478a11d09eb8857
rodrigoo-r <rodrigo@zelixlang.dev>
2025-08-26 23:25:55 -0500

    Add monotonic_resource class for efficient memory allocation

---
+++ include/zelix/memory/monotonic.h
@@ -0,0 +1,53 @@
+/*
+        ==== The Zelix Programming Language ====
+---------------------------------------------------------
+  - This file is part of the Zelix Programming Language
+    codebase. Zelix is a fast, statically-typed and
+    memory-safe programming language that aims to
+    match native speeds while staying highly performant.
+---------------------------------------------------------
+  - Zelix is categorized as free software; you can
+    redistribute it and/or modify it under the terms of
+    the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+---------------------------------------------------------
+  - Zelix is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even
+    the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+---------------------------------------------------------
+  - You should have received a copy of the GNU General
+    Public License along with Zelix. If not, see
+    <https://www.gnu.org/licenses/>.
+*/
+
+//
+// Created by rodrigo on 8/26/25.
+//
+
+#pragma once
+#include "allocator.h"
+#include "resource.h"
+
+namespace zelix::stl::memory
+{
+    template <typename T>
+    class monotonic_resource : public resource<T>
+    {
+        static pmr::lazy_allocator<T> allocator;
+
+    public:
+        template <typename... Args>
+        static T *allocate(Args&&... args) ///< Allocate memory of given size
+        {
+            return allocator.alloc(stl::forward<Args>(args)...);
+        }
+
+        static void deallocate(T *ptr) ///< Deallocate memory at given pointer
+        {
+            return allocator.dealloc(ptr);
+        }
+    };
+}
